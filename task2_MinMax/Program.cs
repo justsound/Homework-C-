39e498c57940f8c09b9a6d919185e074abd51b8f
Just_sound <just_sound@mail.ru>
2023-01-15 11:32:44 +0300

    Вывод наибольшего и наименьшего из 2х чисел

---
+++ task2_MinMax/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите целое первое число ");
+int number1 = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите второе целое число ");
+int number2 = Convert.ToInt32( Console.ReadLine());
+if (number1 > number2)
+{
+    Console.Write("Максимальное число ");
+    Console.WriteLine(number1);
+    Console.Write("Минимальное число ");
+    Console.WriteLine(number2);
+}
+else
+{
+     Console.Write("Максимальное число ");
+    Console.WriteLine(number2);
+    Console.Write("Минимальное число ");
+    Console.WriteLine(number1);
+};
