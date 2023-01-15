12f0c08bfba7a70772f831b2fbde4d013b9adc6a
Just_sound <just_sound@mail.ru>
2023-01-15 12:52:52 +0300

    Вывод последней цифры трехзначного числа

---
+++ task7_lastnumber/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int a = Convert.ToInt32( Console.ReadLine());
+if (99<a && a<1000) Console.WriteLine(a%10);
+else Console.WriteLine("Вы ввели неправильное число");
