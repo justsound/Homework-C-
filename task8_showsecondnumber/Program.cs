6db5fccc592b6d5474e147a9a8f5ad770393c860
Just_sound <just_sound@mail.ru>
2023-01-15 13:09:15 +0300

    Вывод второй цифры трехзначного числа

---
+++ task8_showsecondnumber/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int a = Convert.ToInt32( Console.ReadLine());
+int b = (a / 10) % 10;
+if (99<a && a<1000) Console.WriteLine(b);
+else Console.WriteLine("Вы ввели неправильное число");
