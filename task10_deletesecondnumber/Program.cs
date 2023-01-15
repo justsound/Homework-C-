9878dbebce3dfbc1ccc7623b6a317c32b656857c
Just_sound <just_sound@mail.ru>
2023-01-15 23:39:41 +0300

    Добавил второй вариант удаления второй цифры трехзначного числа

--- task10_deletesecondnumber/Program.cs
+++ task10_deletesecondnumber/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Введите трехзначное число: ");
+﻿/*Console.WriteLine("Введите трехзначное число: ");
 string number = Console.ReadLine();
 Console.Write(number[0]);
-Console.WriteLine(number[2]);
+Console.WriteLine(number[2]);*/
+
+int number = new Random().Next(100, 1000);
+Console.Write(number/100);
+Console.WriteLine(number%10);
