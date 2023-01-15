471c90912e5efd8e923c924cb11696c067d3949e
Just_sound <just_sound@mail.ru>
2023-01-15 11:42:21 +0300

    Определение является ли первое число квадратом второго

---
+++ task2_cheksquare/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое целое число ");
+int number1 = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите второе целое число ");
+int number2 = Convert.ToInt32( Console.ReadLine());
+int square = number2 * number2;
+if (number1 != square) Console.WriteLine("Не Является");
+else Console.WriteLine("Является");
