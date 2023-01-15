9e12ab0d6b7e802200a8ddb39cbe19055f46b6fc
Just_sound <just_sound@mail.ru>
2023-01-15 23:29:16 +0300

    Нахождение третьей цифры

--- task12_thirdnumber/Program.cs
+++ task12_thirdnumber/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число ");
+string a = Console.ReadLine();
+int x = Convert.ToInt32(a);
+if (x > 99) 
+{
+    Console.WriteLine(a[2]);
+}
+else Console.WriteLine("Нет третьей цифры.");
