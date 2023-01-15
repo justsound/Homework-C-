bd1837c39ff8dd8d3aa5da906f5e292d230d27d6
Just_sound <just_sound@mail.ru>
2023-01-15 13:22:28 +0300

    Вывод наибольшей цифры двузначного числа

--- task9_maxnumber/Program.cs
+++ task9_maxnumber/Program.cs
@@ -1,7 +1,11 @@
 ﻿
-Console.WriteLine("Введите трехзначное число ");
+Console.WriteLine("Введите число от 10 до 99");
 string a = Console.ReadLine();
 int b = Convert.ToInt32(a);
-if (99<b && b<1000) Console.WriteLine(a[1]);
+if (9<b && b<100) 
+{
+    if (a[0] > a[1]) Console.WriteLine(a[0]);
+    else Console.WriteLine(a[1]);
+}
 else Console.WriteLine("Вы ввели неправильное число");
 
