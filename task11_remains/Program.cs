2b76f85cd6256aa49733eebce782b31e49b1f09e
Just_sound <just_sound@mail.ru>
2023-01-15 23:55:44 +0300

    Определение кратности

--- task11_remains/Program.cs
+++ task11_remains/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите другое число");
+int b = Convert.ToInt32(Console.ReadLine());
+double c = a%b;
+if (c == 0) Console.WriteLine("Кратные числа");
+else Console.WriteLine( $"Остаток от деления: {c}");
