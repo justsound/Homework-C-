72d8892e07ebe5ea810e2a7cf986429e8f8307e4
Just_sound <just_sound@mail.ru>
2023-01-15 13:17:25 +0300

    Добавили второе решение

--- task8_showsecondnumber/Program.cs
+++ task8_showsecondnumber/Program.cs
@@ -3,3 +3,11 @@ int a = Convert.ToInt32( Console.ReadLine());
 int b = (a / 10) % 10;
 if (99<a && a<1000) Console.WriteLine(b);
 else Console.WriteLine("Вы ввели неправильное число");
+
+/*
+Console.WriteLine("Введите трехзначное число ");
+string a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+if (99<b && b<1000) Console.WriteLine(a[1]);
+else Console.WriteLine("Вы ввели неправильное число");
+*/
--- task9_maxnumber/Program.cs
+++ task9_maxnumber/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите трехзначное число ");
+string a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+if (99<b && b<1000) Console.WriteLine(a[1]);
+else Console.WriteLine("Вы ввели неправильное число");
+
